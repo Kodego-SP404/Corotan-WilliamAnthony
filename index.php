db33da8341853ca39310267752878c102849832e
William Corotan <williamcorotan@gmail.com>
2023-01-23 10:42:38 +0800

    inital commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <?php
+  /* ----- Variables & Data Types ----- */
+
+  /* --------- PHP Data Types --------- */
+  /*
+- String - A string is a series of characters surrounded by quotes
+- Integer - Whole numbers
+- Float - Decimal numbers
+- Boolean - true or false
+- Array - An array is a special variable, which can hold more than one value
+- Object - A class
+- NULL - Empty variable
+- Resource - A special variable that holds a resource
+*/
+
+  /* --------- Variable Rules --------- */
+  /*
+- Variables must be prefixed with $
+- Variables must start with a letter or the underscore character
+- variables can't start with a number
+- Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variables are case-sensitive ($name and $NAME are two different variables)
+*/
+
+
+
+  echo 'hello hi test test234';
+  ?>
+</body>
+
+</html>

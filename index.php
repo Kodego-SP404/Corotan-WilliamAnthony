7e87a035f01e8cd2bac204c79117dd2b6a906a06
William Corotan <williamcorotan@gmail.com>
2023-01-23 17:08:57 +0800

    added super globals

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+/* ---------- Superglobals ---------- */
+//Built in variables that are always available in all scopes
+
+/*
+  $GLOBALS - A superglobal variable that holds information about any variables in global scope.
+  $_GET - Contains information about variables passed through a URL or a form.
+  $_POST -  Contains information about variables passed through a form.
+  $_COOKIE - Contains information about variables passed through a cookie.
+  $_SESSION - Contains information about variables passed through a session.
+  $_SERVER - Contains information about the server environment.
+  $_ENV - Contains information about the environment variables.
+  $_FILES -  Contains information about files uploaded to the script.
+  $_REQUEST - Contains information about variables passed through the form or URL.
+*/
+
+// var_dump($GLOBALS);
+// var_dump($_GET);
+// var_dump($_REQUEST);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -383,10 +404,24 @@ if (condition) {
 
   // Formatted Strings - useful when you have outside data
   // Different specifiers for different data types
-  printf('%s is a %s', 'Ryan', 'nice guy');
+  printf('%s is a %s', 'William', 'nice guy');
   printf('1 + 1 = %f', 1 + 1); // float
 
   ?>
+
+  <ul>
+    <li>Host: <?php echo $_SERVER['HTTP_HOST']; ?></li>
+    <li>Document Root: <?php echo $_SERVER['DOCUMENT_ROOT']; ?></li>
+    <li>System Root: <?php echo $_SERVER['SystemRoot']; ?></li>
+    <li>Server Name: <?php echo $_SERVER['SERVER_NAME']; ?></li>
+    <li>Server Port: <?php echo $_SERVER['SERVER_PORT']; ?></li>
+    <li>Current File Dir: <?php echo $_SERVER['PHP_SELF']; ?></li>
+    <li>Request URI: <?php echo $_SERVER['REQUEST_URI']; ?></li>
+    <li>Server Software: <?php echo $_SERVER['SERVER_SOFTWARE']; ?></li>
+    <li>Client Info: <?php echo $_SERVER['HTTP_USER_AGENT']; ?></li>
+    <li>Remote Address: <?php echo $_SERVER['REMOTE_ADDR']; ?></li>
+    <li>Remote Port: <?php echo $_SERVER['REMOTE_PORT']; ?></li>
+  </ul>
 </body>
 
 </html>

fd8b83a4f6799b532031f2d9c421b89eae077bf4
William Corotan <williamcorotan@gmail.com>
2023-01-24 23:48:25 +0800

    fixed code structure

--- index.php
+++ index.php
@@ -1,111 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-      crossorigin="anonymous"
-    />
-    <title>Leave Feedback</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
-      <div class="container">
-        <a class="navbar-brand" href="#">Kodego SP404</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="/azur/phpdemo/feedback/index.html"
-                >Home</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/azur/phpdemo/feedback/feedback.html"
-                >Feedback</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/azur/phpdemo/feedback/about.html"
-                >About</a
-              >
-            </li>
-          </ul>
+<!-- PHP SCRIPTS -->
+<?php
+session_start();
+
+
+$servername = "localhost";
+$username = "admin";
+$password = "admin";
+$dbname = "php-feedback-activity";
+
+if (isset($_POST['submit'])) {
+
+  $_SESSION['name'] = $_POST['name'];
+  $_SESSION['email'] = $_POST['email'];
+  $_SESSION['body'] = $_POST['body'];
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $insert  = "INSERT INTO feedback (username, email, body, date) VALUES ('" . $_SESSION['name'] . "', '" . $_SESSION['email'] . "', '" . $_SESSION['body'] . "', now())";
+
+    // use exec() because no results are returned
+    $conn->exec($insert);
+    echo "New record created successfully";
+    echo $_SESSION['name'];
+    echo $_SESSION['email'];
+    echo $_SESSION['body'];
+  } catch (PDOException $e) {
+    echo $insert . "<br>" . $e->getMessage();
+  }
+
+  $conn = null;
+}
+?>
+
+<!-- HEADER FILE -->
+<?php
+include './components/header.php';
+
+?>
+
+<body>
+  <?php
+  include './components/navbar.php';
+  ?>
+  <main>
+    <div class="container d-flex flex-column align-items-center">
+      <img src="/php-feedback-activity/img/logo.png" class="w-25 mb-3" alt="" />
+      <h2>Feedback</h2>
+      <p class="lead text-center">Leave feedback for Kodego SP404</p>
+
+
+      <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="mt-4 w-75">
+        <div class="mb-3">
+          <label for="name" class="form-label">Name</label>
+          <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" required />
         </div>
-      </div>
-    </nav>
+        <div class="mb-3">
+          <label for="email" class="form-label">Email</label>
+          <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" required />
+        </div>
+        <div class="mb-3">
+          <label for="body" class="form-label">Feedback</label>
+          <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback" required></textarea>
+        </div>
+        <div class="mb-3">
+          <input type="submit" name="submit" value="Send" class="btn btn-dark w-100" />
+        </div>
+      </form>
+    </div>
+  </main>
 
-    <main>
-      <div class="container d-flex flex-column align-items-center">
-        <img
-          src="/azur/phpdemo/feedback/img/logo.png"
-          class="w-25 mb-3"
-          alt=""
-        />
-        <h2>Feedback</h2>
-        <p class="lead text-center">Leave feedback for Kodego SP404</p>
-        <form action="" class="mt-4 w-75">
-          <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input
-              type="text"
-              class="form-control"
-              id="name"
-              name="name"
-              placeholder="Enter your name"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input
-              type="email"
-              class="form-control"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="body" class="form-label">Feedback</label>
-            <textarea
-              class="form-control"
-              id="body"
-              name="body"
-              placeholder="Enter your feedback"
-            ></textarea>
-          </div>
-          <div class="mb-3">
-            <input
-              type="submit"
-              name="submit"
-              value="Send"
-              class="btn btn-dark w-100"
-            />
-          </div>
-        </form>
-      </div>
-    </main>
+  <?php
+  include './components/footer.php';
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
 
-    <footer class="text-center mt-5">Copyright &copy; 2023</footer>
-
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-      crossorigin="anonymous"
-    ></script>
-  </body>
 </html>
